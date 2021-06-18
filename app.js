2b4a42eac72347fe70f044b669c46ead16298483
Zeeshan Haider <xeieshan@gmail.com>
2021-06-18 22:32:07 +0500

    Upto Date code with Add

--- app.js
+++ app.js
@@ -6,68 +6,68 @@
 // // fs.appendFileSync("notes.txt", "\nBro!!");
 // // fs.appendFileSync("notes.txt", "\nNah!!");
 // console.log(sum);
-const yargs = require('yargs');
-// const validator = require('validator');
-// const chalk = require('chalk');
+const yargs = require("yargs");
+const validator = require('validator');
+const chalk = require("chalk");
 const notes = require("./notes.js");
-// const msg = notes.getNotes();
+const msg = notes.getNote();
 const utils = require("./utils.js");
-// utils.log(chalk.green.bgWhite.bold.inverse(msg));
+utils.log(chalk.green.bgWhite.bold.inverse(msg));
 
-// utils.log(chalk.blue.inverse('isEmail: '),validator.isEmail('ss@ss.com'));
-// utils.log(chalk.red('isUrl: '),validator.isURL('http:test.com'));
+utils.log(chalk.blue.inverse('isEmail: ' + validator.isEmail('ss@ss.com')));
+utils.log(chalk.red('isUrl: ' + validator.isURL('http:test.com')));
 
 // const command = process.argv[2]
 // utils.log(process.argv);
-yargs.version('2.0.0');
+yargs.version('1.1.0');
 // Add Command 
 yargs.command({
-    command : 'add',
-    describe: 'This will add',
-    builder: {
-        title: {
-            describe: 'Note Title',
-            demandOption: true,
-            type: 'string'
-        },
-        description: {
-            describe: 'Note Description',
-            demandOption: true,
-            type: 'string'
-        },
-        
+  command: 'add',
+  describe: 'This will add',
+  builder: {
+    title: {
+      describe: 'Note Title',
+      demandOption: true,
+      type: 'string',
     },
-    handler: function(argv) {
-        notes.addNote( argv.title, argv.description);
-    }
-})
+    body: {
+      describe: 'Note Body',
+      demandOption: true,
+      type: 'string',
+    },
+  },
+  handler: function (s) {
+    utils.log("add handler");
+    notes.addNote(s.title, s.body);
+  },
+});
 
-// Remove Command 
+// Remove Command
 yargs.command({
-    command : 'remove',
-    describe: 'This will remove',
-    handler: function() {
-        utils.log("Removing Note");
-    }
-})
+  command: "remove",
+  describe: "This will remove",
+  handler: function () {
+    utils.log("Removing Note");
+  },
+});
 
-// Create Command 
+// Create Command
 yargs.command({
-    command : 'list',
-    describe: 'This will list all note',
-    handler: function() {
-        utils.log("List all Note");
-    }
-})
+  command: "list",
+  describe: "This will list all note",
+  handler: function () {
+    utils.log("List all Note");
+  },
+});
 
-// Read Command 
+// Read Command
 yargs.command({
-    command : 'list',
-    describe: 'This will read a note',
-    handler: function() {
-        utils.log("Read a Note");
-    }
-})
+  command: "read",
+  describe: "This will read a note",
+  handler: function () {
+    utils.log("Read a Note");
+  },
+});
 
 // utils.log(yargs.argv);
 
--- notes.js
+++ notes.js
@@ -1,25 +1,29 @@
 const fs = require("fs");
 const chalk = require("chalk");
+const utils = require("./utils");
 
 const getNotes = function () {
   return "!!!!!!SUCCESS!!!!!!";
 };
 
-const addNote = function (title, description) {
+const addNotes = function (title, body) {
+  utils.log("AddNote Method");
   const notes = loadNotes();
 
   const found = notes.filter((note) => {
     note.title === title;
   });
+  const length = found.length ;
+  debugger;
   if (found.length === 0) {
     notes.push({
       title: title,
-      description: description,
+      body: body,
     });
     saveNote(notes);
     utils.log(chalk.red("Adding to Notes\n"));
     utils.log("Title: " + title);
-    utils.log("Description: " + description);
+    utils.log("Body: " + body);
     utils.log(chalk.black(notes));
   } else {
     utils.log(chalk.red("Same title already exist in notes!\n"));
@@ -40,4 +44,4 @@ const loadNotes = function () {
     return [];
   }
 };
-module.exports = { getNotes: getNotes, addNote: addNote };
+module.exports = { getNote: getNotes, addNote: addNotes };
